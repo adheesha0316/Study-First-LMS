322a7c6616360348a94dfefe33f4ec865ee04c58
adheesha0316 <adheeshaanuradha2001@gmail.com>
2025-09-14 11:39:51 +0530

    first commit

---
+++ src/main/java/com/campus/lms/CampusLearningManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.campus.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusLearningManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusLearningManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campus/lms/CampusLearningManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusLearningManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
