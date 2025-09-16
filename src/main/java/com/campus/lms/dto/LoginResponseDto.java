c986cfa1ad07c213ccb53798d61f18e0d3959506
adheesha0316 <adheeshaanuradha2001@gmail.com>
2025-09-16 11:30:54 +0530

    create LoginResponseDto & LoginRequestDto

---
+++ src/main/java/com/campus/lms/dto/LoginResponseDto.java
@@ -0,0 +1,18 @@
+package com.campus.lms.dto;
+
+import com.campus.lms.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginResponseDto {
+    private String email;
+    private String token;
+    private Role role;
+    private String userName;
+}
