f7b533295ea6cfe3581248430839eb5ac0008fd4
adheesha0316 <adheeshaanuradha2001@gmail.com>
2025-09-17 08:02:55 +0530

    create UserService and implement methods in UserServiceImpl

--- src/main/java/com/campus/lms/service/Impl/UserServiceImpl.java
+++ src/main/java/com/campus/lms/service/Impl/UserServiceImpl.java
@@ -1,4 +1,26 @@
 package com.campus.lms.service.Impl;
 
-public class UserServiceImpl {
+import com.campus.lms.dto.LoginRequestDto;
+import com.campus.lms.dto.LoginResponseDto;
+import com.campus.lms.entity.User;
+import com.campus.lms.repo.UserRepo;
+import com.campus.lms.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+
+    @Override
+    public User registerUser(LoginRequestDto LoginRequestDto) {
+        return null;
+    }
+
+    @Override
+    public LoginResponseDto login(LoginRequestDto loginRequestDto) {
+        return null;
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return null;
+    }
 }
--- src/main/java/com/campus/lms/service/UserService.java
+++ src/main/java/com/campus/lms/service/UserService.java
@@ -1,4 +1,11 @@
 package com.campus.lms.service;
 
+import com.campus.lms.dto.LoginRequestDto;
+import com.campus.lms.dto.LoginResponseDto;
+import com.campus.lms.entity.User;
+
 public interface UserService {
+    User registerUser(LoginRequestDto LoginRequestDto);
+    LoginResponseDto login(LoginRequestDto loginRequestDto);
+    User findByEmail(String email);
 }
