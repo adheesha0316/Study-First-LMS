cbb438a13be66de31c30e86e03cc059f34a53e51
adheesha0316 <adheeshaanuradha2001@gmail.com>
2025-09-17 11:58:34 +0530

    create UserServiceImpl

--- src/main/java/com/campus/lms/config/AppConfig.java
+++ src/main/java/com/campus/lms/config/AppConfig.java
@@ -2,9 +2,11 @@ package com.campus.lms.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
--- src/main/java/com/campus/lms/service/UserService.java
+++ src/main/java/com/campus/lms/service/UserService.java
@@ -2,10 +2,23 @@ package com.campus.lms.service;
 
 import com.campus.lms.dto.LoginRequestDto;
 import com.campus.lms.dto.LoginResponseDto;
+import com.campus.lms.dto.UserDto;
+import com.campus.lms.dto.UserDtoReturn;
 import com.campus.lms.entity.User;
 
 public interface UserService {
-    User registerUser(LoginRequestDto LoginRequestDto);
+    /**
+     * Register a new user (Student, Lecturer, or Admin)
+     */
+    UserDtoReturn registerUser(UserDto userDto);
+
+    /**
+     * Login a user and generate JWT token
+     */
     LoginResponseDto login(LoginRequestDto loginRequestDto);
+
+    /**
+     * Find user by email
+     */
     User findByEmail(String email);
 }
