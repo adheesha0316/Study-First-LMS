a9cbae7fabbcd8fc4f9c25ee450984e7c9ddaa30
adheesha0316 <adheeshaanuradha2001@gmail.com>
2025-09-24 09:55:30 +0530

    update StudentDto

--- src/main/java/com/campus/lms/dto/StudentDto.java
+++ src/main/java/com/campus/lms/dto/StudentDto.java
@@ -3,12 +3,12 @@ package com.campus.lms.dto;
 import org.springframework.web.multipart.MultipartFile;
 
 public class StudentDto {
-    private String email;   // link User email
+    private String email;       // link User email
     private String name;
     private int age;
     private String nationalId;
     private String phone;
-    private String course;
     private String address;
-    private MultipartFile profileImage;
+    private Integer courseId;   // 🔗 Link to Course
+    private MultipartFile profileImage; // file upload
 }
